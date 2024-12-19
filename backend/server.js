19ecb40d231cf0b6e148e7fd03a2e02b2d03e3fc
Zubair <zubairhussain404@gmail.com>
2024-12-19 14:26:06 +0500

    Server Draft1

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const express = require('express')
+
+
+//express app
+const app = express()
+//middleware
+app.use((req,res,next)=>{
+    console.log(req.path,req.method)
+    next()
+})
+//routes
+app.get('/',(req,res)=>{
+res.json({mssg: 'Welcome To App'})
+})
+//listen to requests
+app.listen(process.env.PORT,()=>{
+    console.log('Listening to port 4000!')
+})
